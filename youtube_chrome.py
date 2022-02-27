6074e7c815c1c6c8bb445c711e5365bf86ccd568
DESKTOP-0CV6H72\dungm <dungvx@mail.com>
2022-02-27 20:13:05 +0700

    2

--- youtube_chrome.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-
-def main():
-    driver = webdriver.Chrome()
-    driver.get("https://www.youtube.com/")
-    soup = BeautifulSoup(content)
-
-main()
